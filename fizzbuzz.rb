ec2367023922b5b339e2ce6eec88ed1c445c3289
RyleeBerger <ryleehaddon24@outlook.com>
2018-06-06 19:06:02 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,31 @@
+# We expect fizzbuzz(3) to return "Fizz"
+# We expect fizzbuzz(5) to return "Buzz"
+# We expect fizzbuzz(15) to return "FizzBuzz"
+# We expect fizzbuzz(4) to return "nil".
+
+def fizzbuzz(number)
+  if number % 3
+    puts "Fizz"
+  elsif
+  
+  fizz(3)
+  
+  def buzz(number)
+    if number % 5
+puts "Buzz"
+end
+
+buzz(5)
+
+def fizzbuzz(number)
+  if number % 3, 5
+    puts "FizzBuzz"
+  end
+  
+  fizzbuzz(3, 5)
+  
+  def fizzbuzz4(number)
+    if number % 4
+      puts "Nil"
+    end
+  
